059b0ede305803301d2d30e64a738c45e28ca227
rlo <renato.loeffel@zuehlke.com>
2012-09-10 18:42:18 +0200

    baseline

---
+++ com.zuehlke.zfb.model/src/main/java/com/zuehlke/zfb/model/RootModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zuehlke.zfb.model;
+
+import java.io.File;
+
+/**
+ *
+ * @author rlo
+ */
+public class RootModel {
+
+    private File currentDirectory;
+
+    public File getCurrentDirectory() {
+        return currentDirectory;
+    }
+
+    public void setCurrentDirectory(File currentDirectory) {
+        this.currentDirectory = currentDirectory;
+    }
+}
