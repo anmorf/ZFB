b2224e4c747391c67c84924044d4db80819d5d93
asw <asw@ZRHN0896.ads.zuehlke.com>
2012-09-12 16:58:39 +0200

    TreeView sets current Directory path

--- com.zuehlke.zfb.control/src/com/zuehlke/zfb/control/TreeControl.java
+++ com.zuehlke.zfb.control/src/com/zuehlke/zfb/control/TreeControl.java
@@ -4,12 +4,17 @@
  */
 package com.zuehlke.zfb.control;
 
+import com.zuehlke.zfb.model.RootModel;
 import com.zuehlke.zfb.model.TreeModel;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -17,13 +22,29 @@ import javafx.scene.control.TreeView;
  */
 public class TreeControl implements Initializable {
 
-    private TreeModel treeModel = TreeModel.getInstance();
     @FXML
     private TreeView<String> tree;
 
+    private TreeModel treeModel = TreeModel.getInstance();
+    private RootModel rootModel = RootModel.getInstance();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tree.setRoot(treeModel.getRoot());
+        tree.setShowRoot(false);
+        tree.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                rootModel.setCurrentDirectory(new File(getFullPath(tree.getSelectionModel().getSelectedItem())));
+            }
+        });
     }
 
+    private String getFullPath(TreeItem<String> selectedItem) {
+        if (selectedItem.getParent().getValue().equals("root")) {
+            return selectedItem.getValue();
+        }
+        String path = getFullPath(selectedItem.getParent());
+        return  path + (path.endsWith(File.separator) ? "" : File.separator) + selectedItem.getValue();
+    }
 }
