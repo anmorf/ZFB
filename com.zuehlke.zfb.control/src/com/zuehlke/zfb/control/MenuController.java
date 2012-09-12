1e32eae5b59c23f4022975bd8b476ef5fb5a9a8f
bdo <bdo@ZRHN1321.ads.zuehlke.com>
2012-09-12 18:13:19 +0200

    added menu

---
+++ com.zuehlke.zfb.control/src/com/zuehlke/zfb/control/MenuController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zuehlke.zfb.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Popup;
+
+
+public class MenuController implements Initializable {
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+}
