a4d58d67d04f7baf63fd645ea946a6ab976bb6c8
Heinz Marti <hem@zuehlke.com>
2012-09-13 14:21:04 +0200

    remove borderpane

--- com.zuehlke.zfb.control/src/com/zuehlke/zfb/control/ContentControl.java
+++ com.zuehlke.zfb.control/src/com/zuehlke/zfb/control/ContentControl.java
@@ -1,11 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.zuehlke.zfb.control;
 
 import com.zuehlke.zfb.model.RootModel;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,7 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.SplitPane;
 
 /**
  *
@@ -28,13 +23,13 @@ public class ContentControl implements Initializable,  ChangeListener<String>  {
     private RootModel rootModel = RootModel.getInstance();
     
     @FXML
-    private BorderPane content;
+    private SplitPane content;
     
 
     private void loadContent(String centerContent) {
         try {
             Node node = (Node) FXMLLoader.load(getClass().getResource(centerContent));
-            content.centerProperty().setValue(node);
+            content.getItems().set(1, node);
         } catch (IOException ex) {
             Logger.getLogger(ContentControl.class.getName()).log(Level.SEVERE, null, ex);
         }
